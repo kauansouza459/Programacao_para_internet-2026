883447f1211fa4a127c164d3acc7b3fa6e1881c5
Gamahen011 <ricaohgp@gmail.com>
2026-04-09 21:30:35 -0300

    Melhora a validação do formulário e adiciona alertas para feedback do usuário

--- roteiro_02/assents/js/scprpt.js
+++ roteiro_02/assents/js/scprpt.js
@@ -1,10 +1,36 @@
-//alert('ola seja bem vindo ao meu mini-site, espero que goste!');
-
-
 const form = document.querySelector("form");
 
- form.addEventListener("submit", function(event) {
- event.preventDefault();
- alert("Formulário enviado!");
- console.log("Formulário enviado!");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+
+  console.log(nome);
+  console.log(email);
+
+  alert(`Formulário enviado!\nNome: ${nome}\nEmail: ${email}`);
+  
+
+  if (nome.length < 3) {
+     alert("O nome deve ter pelo menos 3 caracteres.");
+     return;
+    }
+   if (!email.includes("@")) {
+       alert("Email inválido.");
+       return;
+    
+    }
+  console.log("Formulário enviado!");
 });
+
+
+document.getElementById("bAlerta").addEventListener("click", function() {
+  alert("Este é um alerta em javascript!");
+});
+
+document.getElementById("bCor")
+.addEventListener("click", function() {
+document.body.style.backgroundColor = "rgb(82, 0, 0)";
+});
+
